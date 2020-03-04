06a53fb9f38be7975775591cf4225b0415a1f698
Luca Guidi <me@lucaguidi.com>
2020-03-04 11:09:53 +0100

    Add $logger for debugging purposes

--- handler.rb
+++ handler.rb
@@ -2,11 +2,18 @@
 
 require_relative "./aws/lambda"
 require_relative "./app"
+require "logger"
 
 # rubocop:disable Style/GlobalVars
 $app ||= App.new
+$logger ||= Logger.new($stdout)
 
 def handler(event:, context:)
+  # Useful for debugging:
+  #
+  # $logger.info(event)
+  # $logger.info(AWS::Lambda::Rack.env_for(event))
+
   AWS::Lambda.call($app, event, context)
 end
 # rubocop:enable Style/GlobalVars
